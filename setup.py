6aca0937423bb01172d1151b6a8ea2c22cbfb6dc
wmpjrufg <55900120+wmpjrufg@users.noreply.github.com>
2021-10-06 14:58:54 -0300

    Organização pasta
    Pasta organização

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'FINITO_FEM_TOOLBOX',
-    version = '0.0.0',
+    version = '0.0.1',
     author = 'Prof. Wanderlei M. Pereira Junior, Prof. Davidson de Oliveira França Junior, Prof. Marcos Rabelo Napoleão, Civil Eng. Gabriel B. Carvalho, Civil Eng. José Vitor Carvalho and Civil Eng. Murilo Carneiro Rodrigues',
     author_email = 'wanderlei_junior@ufcat.edu.br',
     url='https://wmpjrufg.github.io/FINITO_TOOLBOX/',
